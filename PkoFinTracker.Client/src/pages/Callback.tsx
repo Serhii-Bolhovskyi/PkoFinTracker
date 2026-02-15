13d625b528da238557c97d2d9447cb5d1f14ddaa
Serhii-Bolhovskyi <azeror72@gmail.com>
2026-02-15 01:49:48 +0100

    Update ui. Add animations to Callback page

--- PkoFinTracker.Client/src/pages/Callback.tsx
+++ PkoFinTracker.Client/src/pages/Callback.tsx
@@ -1,6 +1,9 @@
 import {useNavigate, useSearchParams} from "react-router-dom";
 import {useEffect} from "react";
+import circlesAnimated from "../assets/circles-animated.b14dbffd.svg"
+
 import axios from "axios";
+
 
 export const Callback = () => {
     const [searchParams] = useSearchParams();
@@ -16,15 +19,25 @@ export const Callback = () => {
         }
     }, [code])
     return (
-        <div className="fixed inset-0 bg-bank-main flex items-center justify-center">
-            <video
-                src="/src/assets/callback_animation.mp4"
-                autoPlay
-                muted
-                loop
-                playsInline
-                className="w-full h-full"
+        <div className="relative h-screen w-full bg-bank-main overflow-hidden flex items-center justify-center">
+            <img
+                src={circlesAnimated}
+                alt=""
+                className="absolute inset-0 w-full h-full object-cover opacity-30"
             />
+            
+            <div className="flex flex-col items-center justify-between w-full h-screen z-10 text-center p-16 space-y-5">
+                
+                <h1 className="text-4xl text-white opacity-60 animate-slide-up mt-20">
+                    Track your balance, spending, 
+                    and progress with ease
+                </h1>
+                
+                {/* Title */}
+                <h2 className="text-white text-3xl font-semibold mb-2 tracking-wider animate-shimmer">
+                    Loading...
+                </h2>
+            </div>
         </div>
-    )
+    );
 }
