9143fb482eed9225fca11508e0eeea64aeff88db
Serhii-Bolhovskyi <azeror72@gmail.com>
2026-02-15 18:57:24 +0100

    Compare income and expense with prec month. Upd UI

---
+++ PkoFinTracker.Client/src/components/Cashflow.tsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import type {TransactionProps} from "./TransactionTable.tsx";
+import {useMemo} from "react";
+import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recharts";
+import {RechartsDevtools} from "@recharts/devtools";
+
+const Cashflow: React.FC<TransactionProps> = ({transactions}) => {
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    const chartData = useMemo(() => {
+
+        const data = monthNames.map(name => ({
+            name: name,
+            income: 0,
+            expense: 0
+        }));
+
+        transactions.forEach(t => {
+            const date = new Date(t.bookingDate)
+
+            const monthIndex = date.getMonth();
+
+            if(t.indicator === "CRDT"){
+                data[monthIndex].income += t.amount;
+            } else {
+                data[monthIndex].expense += Math.abs(t.amount);
+            }
+        })
+        console.log(data)
+        return data;
+    }, [transactions]);
+
+    return (
+        <div className="col-span-8 row-span-2 col-start-5 bg-bank-comp rounded-2xl p-5">
+            {/*<p className="text-2xl text-white">Cashflow</p>*/}
+            <SimpleBarChart data={chartData} />
+        </div>
+
+    )
+}
+
+class SimpleBarChart extends React.Component<{ data: any }> {
+    render() {
+        let {data} = this.props;
+        return (
+            <BarChart
+                style={{width: '100%', maxHeight: '45vh', aspectRatio: 1.618}}
+                responsive
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 0,
+                    left: 0,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="name"/>
+                <YAxis width="auto"/>
+                <Tooltip/>
+                <Legend/>
+                <Bar name="Income" dataKey="income" fill="#A855F7" radius={[4, 4, 0, 0]}/>
+                <Bar name="Expense" dataKey="expense" fill="#DAA520" radius={[4, 4, 0, 0]}
+                />
+                <RechartsDevtools/>
+            </BarChart>
+        );
+    }
+}
+
+export default Cashflow;
--- PkoFinTracker.Client/src/pages/Dashboard.tsx
+++ PkoFinTracker.Client/src/pages/Dashboard.tsx
@@ -2,12 +2,12 @@ import * as React from "react";
 import type {Transaction} from "../types/Transaction.ts";
 import {useEffect, useMemo} from "react";
 import axios from "axios";
-import TransactionTable, {type TransactionProps} from "../components/TransactionTable.tsx";
 import AccountCard, {type AccountCardProps} from "../components/AccountCard.tsx";
 import type {BankAccount} from "../types/BankAccount.ts";
 
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import { RechartsDevtools } from '@recharts/devtools';
+import { ArrowDownToLine, CalendarDays } from 'lucide-react';
+import Cashflow from "../components/Cashflow.tsx";
+import TransactionTable from "../components/TransactionTable.tsx";
 
 
 export const Dashboard: React.FC = () => {
@@ -15,18 +15,54 @@ export const Dashboard: React.FC = () => {
     const [recentTransactions, setRecentTransactions] = React.useState<Transaction[]>([]);
     const [accounts, setAccounts] = React.useState<BankAccount[]>([]);
 
-    const { totalIncome, totalExpense } = useMemo(() => {
-        return transactions.reduce(
+    const { totalIncome, totalExpense, incomeDiff, expenseDiff } = useMemo(() => {
+        // current date
+        const currMonth = new Date().getMonth();
+        const currYear = new Date().getFullYear();
+        
+        // prev date
+        const prevDate = new Date(currYear, currMonth - 1, 1);
+        const prevMonth = prevDate.getMonth();
+        const prevYear = prevDate.getFullYear();
+   
+        const stats = transactions.reduce(
             (acc, t) => {
-                if (t.indicator === 'CRDT') {
-                    acc.totalIncome += t.amount;
-                } else if (t.indicator === 'DBIT') {
-                    acc.totalExpense += t.amount;
+                const tDate = new Date(t.bookingDate);
+                const tMonth = tDate.getMonth();
+                const tYear = tDate.getFullYear();
+                
+                // current month
+                if(tMonth === currMonth && tYear === currYear){
+                    if (t.indicator === 'CRDT') {
+                        acc.currInc += t.amount;
+                    } else if (t.indicator === 'DBIT') {
+                        acc.currExp += t.amount;
+                    }
                 }
+                
+                // prev month
+                if(tMonth === prevMonth && tYear === prevYear){
+                    if (t.indicator === 'CRDT') {
+                        acc.prevInc += t.amount;
+                    } else if (t.indicator === 'DBIT') {
+                        acc.prevExp += t.amount;
+                    }
+                }
+               
                 return acc;
             },
-            { totalIncome: 0, totalExpense: 0 }
+            { currInc: 0, currExp: 0, prevInc: 0, prevExp: 0 },
         );
+        
+        const calcDiff = (curr: number, prev: number) => 
+            prev === 0 ? 0 : ((curr - prev)/ prev) * 100;
+        
+        return { 
+            totalIncome: stats.currInc,
+            totalExpense: stats.currExp,
+            incomeDiff: calcDiff(stats.currInc, stats.prevInc),
+            expenseDiff: calcDiff(stats.currExp, stats.prevExp),
+        }
     }, [transactions]);
     
     useEffect(() => {
@@ -56,10 +92,10 @@ export const Dashboard: React.FC = () => {
                 <Cashflow transactions={transactions} />
                 {accounts.length > 0 && <>
                     <div className="col-start-1 col-span-2">
-                        <StatCard title="Total Income" amount={totalIncome} currency={accounts[0].currency} type='income' />
+                        <StatCard title="Total Income" amount={totalIncome} diff={incomeDiff} currency={accounts[0].currency} type='income' />
                     </div>
                     <div className="col-span-2">
-                        <StatCard title="Total Expense" amount={totalExpense} currency={accounts[0].currency} type='expense' />
+                        <StatCard title="Total Expense" amount={totalExpense} diff={expenseDiff} currency={accounts[0].currency} type='expense' />
                     </div>
                 </>}
                 <TransactionTable  transactions={recentTransactions}/>
@@ -70,6 +106,7 @@ export const Dashboard: React.FC = () => {
 interface StatCardProps {
     title: string,
     amount: number,
+    diff: number,
     currency: string,
     type: 'income' | 'expense'
     
@@ -83,8 +120,9 @@ const CURRENCY_SYMBOLS: Record<string, string> = {
     'PLN': 'zł'
 };
 
-const StatCard: React.FC<StatCardProps> = ({title, amount, currency, type}) => {
+const StatCard: React.FC<StatCardProps> = ({title, amount, diff, currency, type}) => {
     const isIncome = type === 'income';
+    const isPositive = diff > 0;
     const currencySymbol = CURRENCY_SYMBOLS[currency] || currency;
     return (
         <div className="bg-bank-comp p-5 rounded-2xl flex flex-col">
@@ -92,88 +130,46 @@ const StatCard: React.FC<StatCardProps> = ({title, amount, currency, type}) => {
                 {title}
             </span>
             
-            <div className="flex items-baseline gap-2 mt-1">
+            <div className="flex items-baseline mt-1">
                 <h3 className="text-2xl font-semibold text-white">
                     {currencySymbol}{amount.toLocaleString('en-US', { minimumFractionDigits: 2 })}
                 </h3>
             </div>
             
-            <div className={`mt-3 flex items-center gap-1 font-bold ${isIncome ? 'text-emerald-500' : 'text-purple-400'}`}>
-                <span>{isIncome ? '↑' : '↓'}</span>
-                <span>{isIncome ? 'Income' : 'Spending'}</span>
+            <div className="mt-3 flex items-center start flex-wrap gap-1 ">
+                <div className={`flex font-bold text-lg space-x-1 ${isIncome ? 'text-emerald-500' : 'text-red-600'}`}>
+                    <span>{isPositive ? '↑' : '↓'} </span>
+                    <span>{diff.toFixed(1)}%</span>
+                </div>
+                <span className="text-white opacity-60"> From last month</span>
             </div>
         </div>
     )
 }
 
-const Cashflow: React.FC<TransactionProps> = ({transactions}) => {
-    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    
-    const chartData = useMemo(() => {
-        
-        const data = monthNames.map(name => ({
-            name: name,
-            income: 0,
-            expense: 0
-        }));
-        
-        transactions.forEach(t => {
-            const date = new Date(t.bookingDate)
-            
-            const monthIndex = date.getMonth();
-            
-            if(t.indicator === "CRDT"){
-                data[monthIndex].income += t.amount;
-            } else {
-                data[monthIndex].expense += Math.abs(t.amount);
-            }
-        })
-        
-        return data;
-    }, [transactions]);
-    
-    return (
-        <div className="col-span-8 row-span-2 col-start-5 bg-bank-comp rounded-2xl p-5">
-            {/*<p className="text-2xl text-white">Cashflow</p>*/}
-            <SimpleBarChart data={chartData} />
-        </div>
-        
-    )
-}
-
-const SimpleBarChart = ({data}) => {
-    return (
-        <BarChart
-            style={{ width: '100%', maxHeight: '45vh', aspectRatio: 1.618 }}
-            responsive
-            data={data}
-            margin={{
-                top: 5,
-                right: 0,
-                left: 0,
-                bottom: 5,
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3"/>
-            <XAxis dataKey="name" />
-            <YAxis width="auto" />
-            <Tooltip />
-            <Legend />
-            <Bar name="Income" dataKey="income" fill="#A855F7" radius={[4, 4, 0, 0]} />
-            <Bar name="Expense"  dataKey="expense" fill="#DAA520"  radius={[4, 4, 0, 0]}
-            />
-            <RechartsDevtools />
-        </BarChart>
-    );
-};
-
-
 const Greetings: React.FC<AccountCardProps> = ({accounts}) =>{
     const account = accounts[0];
+    const currDate = new Date().toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+    }).replace(',', '');
     return (
-        <div className="col-span-12 text-white mb-2">
-           <h1 className="text-2xl">Good day, {accounts.length > 0 && account.name} </h1>
-           <p className="text-lg text-white opacity-60">Track your balance, spending, and progress with ease</p> 
+        <div className="flex col-span-12 text-white mb-2 justify-between items-end">
+            <div className="flex flex-col">
+                <h1 className="text-2xl">Good day, {accounts.length > 0 && account.name} </h1>
+                <p className="text-lg text-white opacity-60">Track your balance, spending, and progress with ease</p>
+            </div>
+            <div className="flex gap-3">
+                <div className="flex items-center gap-2 rounded-lg px-4 py-2 bg-bank-comp">
+                    <CalendarDays className="w-5 h-5" />
+                    <span>{currDate}</span>
+                </div>
+                <button className="flex items-center gap-2 bg-bank-purple rounded-lg px-4 py-2 hover:bg-purple-700 transition">
+                    <ArrowDownToLine className="w-5 h-5" />
+                    <span>Export</span>
+                </button>
+            </div>
         </div>
     )
 } 
